b6716a8cf667b7b836e164ce8d5be51782188f72
vivek <wewakeverma1990@gmail.com>
2023-11-30 07:46:36 +0530

    fixed second pre deployment error for verifyUser

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { test, updateUser, deleteUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/VerifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
