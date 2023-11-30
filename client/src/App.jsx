25b3c0300f42136ffc089da07bcf9f7ebbbb1896
vivek <wewakeverma1990@gmail.com>
2023-11-30 07:38:00 +0530

    fixed deployment error for SignIn App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Signin from './pages/Signin';
+import Signin from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
